b5f9485bec10cdf6c072373a1d57e8620092693d
doSMgochi <aenarchi@gmail.com>
2024-07-23 11:15:31 +0900

    array

---
+++ hello-002/index.js
@@ -0,0 +1,5 @@
+// arrays.js 에서 export 한 arrays 함수를 index.js 에서 사용하겠다
+const { arrays, multiArrays } = require("./arrays");
+// import 한 함수를 실행하기
+arrays();
+multiArrays();
---
+++ hello-003/index.js
@@ -0,0 +1,20 @@
+import {
+  addNation,
+  arrays,
+  multiArrays,
+  viewNations,
+  addNums,
+  expoNum,
+} from "./arrays.js";
+
+arrays();
+multiArrays();
+
+addNation("대한민국");
+addNation("미국");
+addNation("영국");
+addNation("프랑스");
+
+viewNations();
+addNums();
+expoNum();
